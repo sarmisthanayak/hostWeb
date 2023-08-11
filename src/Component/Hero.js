40928be46fcf0b43ca6faf7f779d51c9a3a7f1d9
Sarmistha Nayak <er.sarmi2310@gmail.com>
2023-08-11 17:36:58 +0530

    file updated

--- src/Component/Hero.js
+++ src/Component/Hero.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
 const HeroSec = styled.section`
-background-color: #15212a !important;
-padding: 135px 0;
-z-index: 0;
-position: relative;
+  background-color: #15212a !important;
+  padding: 60px 0;
+  z-index: 0;
+  position: relative;
 `;
 
 const HeroWrapper = styled.div`
-font-family: sans-serif !important;
+  font-family: sans-serif !important;
 `;
 
 const VideoBackground = styled.video`
@@ -31,80 +30,74 @@ const Overlay = styled.div`
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0);
-  z-index: -1; 
+  z-index: -1;
 `;
 
-
-
 const Title = styled.h1`
-margin-top: 0!important;
-margin-bottom: 20px!important;
-text-transform: capitalize!important;
-position: relative!important;
-display: inline-block!important;
-line-height: 60px!important;
-color: #fff;
-font-size: 3rem;
+  margin-top: 0 !important;
+  margin-bottom: 10px !important;
+  text-transform: capitalize !important;
+  position: relative !important;
+  display: inline-block !important;
+  line-height: 40px !important; 
+  color: #fff;
+  font-size: 2rem; 
 `;
 
 const Description = styled.p`
-position: relative!important;
-color: #fff;
-font-size: 20px!important;
-line-height: 34px!important;
-text-align: center;
-font-size: 2rem;
+  position: relative !important;
+  color: #fff;
+  font-size: 16px !important; 
+  line-height: 28px !important; 
+  text-align: center;
 `;
 
 const Buttons = styled.div`
-  margin-top: 20px;
+  margin-top: 15px; 
 
   @media (max-width: 768px) {
     display: flex;
-    flex-direction: row;
+    flex-direction: column; 
     align-items: center;
     justify-content: center;
-    margin-top: 30px;
+    margin-top: 20px; 
   }
 `;
 
 const Button = styled.button`
-background-color: #F33A6A;
-border: 2px solid blue;
-border-radius: 50px 20px;
-color: white;
-padding: 10px 30px;
-text-align: center;
-text-decoration: none;
-display: inline-block;
-font-size: 16px;
-margin: 50px 10px;
-cursor: pointer;
+  background-color: #f33a6a;
+  border: 2px solid blue;
+  border-radius: 50px 20px;
+  color: white;
+  padding: 8px 20px; 
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 14px; 
+  margin: 10px 0; 
+  cursor: pointer;
 `;
 
 const Image = styled.div`
   img {
     max-width: 100%;
-    height: 80%;
-   
-    @media (max-width: 768px) {
-      display: none;
+    height: auto; 
+    display: block; 
+    margin: 0 auto; 
   }
-}
 `;
 
 const HeroSection = () => {
   return (
-      <HeroSec>
-        <VideoBackground autoPlay loop muted>
-            <source src="http://inebur.com/antler/template/assets/videos/planet.mp4" type="video/mp4" />
-          </VideoBackground>
-        <HeroWrapper>
-          
-          <Overlay />
-          <div className='container'>
-            <div className='row'>
-              <div className='col-md-6 col-sm-6 pt-5 pb-1 text-center'>
+    <HeroSec>
+      <VideoBackground autoPlay loop muted>
+        <source src="http://inebur.com/antler/template/assets/videos/planet.mp4" type="video/mp4" />
+      </VideoBackground>
+      <HeroWrapper>
+        <Overlay />
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-6 col-sm-12 pt-4 pb-1 text-center'> 
               <Title>Welcome to Flup.in Hosting Website</Title>
               <Description>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fringilla tincidunt urna, at tempor libero.
@@ -113,17 +106,16 @@ const HeroSection = () => {
                 <Button>Learn More</Button>
                 <Button>Get Started</Button>
               </Buttons>
-              </div>
-              <div className='col-md-6 col-sm-6 text-center'>
-                <Image>
-                  <img src="https://inebur.com/antler/template/assets/patterns/domainmanage.svg" alt="Hero" />
-                </Image>
-              </div>
             </div>
-            
+            <div className='col-md-6 col-sm-12 text-center'>
+              <Image>
+                <img src="https://inebur.com/antler/template/assets/patterns/domainmanage.svg" alt="Hero" />
+              </Image>
+            </div>
           </div>
-        </HeroWrapper>
-      </HeroSec>
+        </div>
+      </HeroWrapper>
+    </HeroSec>
   );
 };
 
