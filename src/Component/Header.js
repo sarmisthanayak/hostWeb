6a23f158a06d9fcb4783871f9f6fc0bd54ee430f
Sarmistha Nayak <er.sarmi2310@gmail.com>
2023-08-11 17:56:48 +0530

    file updated

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -6,34 +6,50 @@ import { styled } from 'styled-components';
 const Header = () => {
 
   return (
-    <header id="header" class="fixed-top">
-      <div class="container d-flex align-items-center justify-content-between">
-      <img src="http://inebur.com/antler/template/assets/img/logo.svg" alt="YourLogo" />
-      <label for="menu">&#9776;</label>
-      <input type="checkbox" id="menu"/>
-      <nav id="navbar" class="navbar">
-        
-              <ul>
-                <li>
-                  <a class="nav-link active" aria-current="page" href="#">Menu1</a>
+    <header id="header" className="fixed-top">
+      <div className="container-fluid">
+        <div className="row align-items-center justify-content-between">
+          <div className="col-md-3">
+            <img src="http://inebur.com/antler/template/assets/img/logo.svg" alt="YourLogo" />
+          </div>
+          <div className="col-md-2">
+            <label htmlFor="menu">&#9776;</label>
+            <input type="checkbox" id="menu" />
+          </div>
+          <div className="col-md-7">
+            <nav id="navbar" className="navbar">
+              <ul className="d-flex justify-content-end">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Menu1
+                  </a>
                 </li>
-                <li>
-                  <a class="nav-link" href="#">Menu2</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Menu2
+                  </a>
                 </li>
-                <li>
-                  <a class="nav-link" href="#">Menu3</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Menu3
+                  </a>
                 </li>
-                <li>
-                  <a class="nav-link" href="#">Menu4</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Menu4
+                  </a>
                 </li>
-                <li>
-                  <a class="nav-link" href="#">Login</a>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Login
+                  </a>
                 </li>
               </ul>
             </nav>
           </div>
-        
-        </header>
+        </div>
+      </div>
+    </header>
   );
 };
 
