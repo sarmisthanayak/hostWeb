22781e1474352e62dfea78ab3e825585b21b1c41
Sarmistha Nayak <er.sarmi2310@gmail.com>
2023-08-11 18:42:30 +0530

    file updated

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,53 +1,126 @@
-import React from 'react';
-import './design.css';
-import { styled } from 'styled-components';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import MenuIcon from '@material-ui/icons/Menu';
 
+const HeaderContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  padding: 25px;
+  background-color: #000;
+
+
+  @media (max-width: 768px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
+  }
+`;
+
+const Logo = styled.div`
+  font-size: 1.5rem;
+  justify-self: start;
+
+  @media (max-width: 768px) {
+    
+  }
+`;
+const LogoImg = styled.img`
+  height: 30px; /* Adjust the height as needed */
+`;
+
+const MenuContainer = styled.div`
+  display: flex;
+  justify-content: center;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const MenuItem = styled.button`
+  color: inherit;
+  background: none;
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+  font-size: 1rem;
+`;
+
+const LoginContainer = styled.div`
+  justify-self: end;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const HamburgerIcon = styled(MenuIcon)`
+  display: none;
+  justify-self: end;
+  cursor: pointer;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
+
+const DrawerContainer = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    background-color: #333;
+    padding: 10px;
+  }
+`;
+
+const DrawerItem = styled.button`
+  color: #fff;
+  background: none;
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+  font-size: 1rem;
+`;
 
 const Header = () => {
+  const [showDrawer, setShowDrawer] = useState(false);
+
+  const menuItems = ['Menu 1', 'Menu 2', 'Menu 3', 'Menu 4'];
+
+  const toggleDrawer = () => {
+    setShowDrawer(!showDrawer);
+  };
 
   return (
-    <header id="header" className="fixed-top">
-      <div className="container-fluid">
-        <div className="row align-items-center">
-          <div className="col-md-5 col-6">
-            <img src="http://inebur.com/antler/template/assets/img/logo.svg" alt="YourLogo" />
-          </div>
-          <div className="col-md-7 col-6 text-md-end">
-            <label htmlFor="menu">&#9776;</label>
-            <input type="checkbox" id="menu" />
-            <nav id="navbar" className="navbar">
-              <ul className="d-flex justify-content-end">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Menu1
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Menu2
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Menu3
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Menu4
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Login
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </header>
+    <>
+      <HeaderContainer>
+        <Logo><LogoImg src="http://inebur.com/antler/template/assets/img/logo.svg" alt="YourLogo" /></Logo>
+        <MenuContainer>
+          {menuItems.map((item, index) => (
+            <MenuItem key={index}>{item}</MenuItem>
+          ))}
+        </MenuContainer>
+        <LoginContainer>
+          <button>Login</button>
+        </LoginContainer>
+        <HamburgerIcon onClick={toggleDrawer} />
+      </HeaderContainer>
+      {showDrawer && (
+        <DrawerContainer>
+          {menuItems.map((item, index) => (
+            <DrawerItem key={index}>{item}</DrawerItem>
+          ))}
+          <DrawerItem>Login</DrawerItem>
+        </DrawerContainer>
+      )}
+    </>
   );
 };
 
